d849fec1d51d5d645a096b10d0c821586087ad31
Paige <pvanditta@comcast.net>
2024-10-18 14:08:52 -0500

    stopped at 11.4

--- Slot_Machine_lesson_3_Chpt11.R
+++ Slot_Machine_lesson_3_Chpt11.R
@@ -1,7 +1,7 @@
 # Chpt 11, Loops. Calculate the expected pay-rate of the slot machine
 # copied code to create the slot machine
 get_symbols<- function(){
-  wheel<- c("DD", '7', 'BBB',"BB", "B", "c", '0')
+  wheel<- c("DD", '7', 'BBB',"BB", "B", "C", '0')
   sample(wheel, size =3, replace = TRUE,
          prob = c(0.03, 0.03,0.06, 0.1, 0.25, 0.01, 0.52))
 }
@@ -14,18 +14,17 @@ score<- function(symbols){
   #get prize
   if(same) { 
     payouts_same <- c("DD"= 100, '7'= 80, 'BBB' = 40,"BB" = 25, 
-                      "B" = 10, "c" = 10, '0' = 0 )
+                      "B" = 10, "C" = 10, '0' = 0 )
     prize<- unname(payouts_same[symbols[1]])
   } else if (all(bars)) { 
     prize <- 5 
   } else {
-    cherries <- sum(symbols == "c")
+    cherries <- sum(symbols == "C")
     prize<- c(0,2,5)[cherries+1]
   }
   #adjust for diamonds
   diamonds <- sum(symbols == "DD")
-  if (diamonds == 0) {diamonds <- diamonds +1}
-  prize* diamonds^ 2
+  prize*2 ^ diamonds
 }
 score(symbols)
 #play slots
@@ -76,6 +75,9 @@ sum(slot_results$probability)
 for (value in c("my", "first", "for", "loop")){
   print("one run")
 }
+#for (value in that){
+#this
+#}
 #the that object is a set of objects. the code in between the braces  will run once for each member of that. 
 #runs one print for each value in "my first for loop"
 #can use any symbol you like.
@@ -86,4 +88,51 @@ for (i in 1:4){
   chars[i] <- words[i]
 }
 chars
-#create  a for loop for 
+#create  a for loop for calculating the prize in each row of slot_results. 
+slot_results$prize <- NA
+head(slot_results, 3)
+for (i in 1:nrow(slot_results)){
+  symbols <- c(slot_results[i,1], slot_results[i,2], slot_results[i,3])
+  slot_results$prize[i] <-score(symbols)
+}
+#expected payout from playing. sum of slot_results$prize * prob of winning that prize. 
+
+sum(slot_results$prize * slot_results$probability)
+slot_results[286,]
+# try to handle write a version of score where DD is both wild and doubler.
+#can't figure out on my own yet, learn from book code. 
+symbols<- get_symbols()
+score<- function(symbols){
+  #identify case
+  diamonds <- sum(symbols == "DD")
+  cherries <- sum(symbols == "C")
+  #sum number of diamonds in a row
+  slots <- symbols[symbols != "DD"]
+  same <- length(unique(slots)== 1)
+  bars <- slots %in% c("B", "BB", "BBB")
+  
+  #get prize
+  if(diamonds ==3){
+    prize <- 100
+  } else if(same) { 
+    payouts_same <- c('7'= 80, 'BBB' = 40,"BB" = 25, 
+                      "B" = 10, "C" = 10, '0' = 0 )
+    prize<- unname(payouts_same[slots[1]])
+  } else if (all(bars)) { 
+    prize <- 5 
+  } else if (cherries >0){
+    prize<- c(0,2,5)[cherries + diamonds + 1]
+  } else {
+    prize <- 0 }
+  
+
+  prize*2 ^ diamonds
+}
+score(symbols)
+# recalculate the expected values:
+for (i in 1:nrow(slot_results)){
+  symbols <- c(slot_results[i,1], slot_results[i,2], slot_results[i,3])
+  slot_results$prize[i] <- score(symbols)
+}
+sum(slot_results$prize * slot_results$probability)
+#wrong, try again. 
