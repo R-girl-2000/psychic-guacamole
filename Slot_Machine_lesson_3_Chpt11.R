d26d9588abf217cd8b29f5368a582f18a89afd33
R-girl-2000 <pvanditta@comcast.net>
2024-10-24 13:37:06 -0500

    finsihed chapter 11, most of 12

--- Slot_Machine_lesson_3_Chpt11.R
+++ Slot_Machine_lesson_3_Chpt11.R
@@ -1,5 +1,6 @@
 # Chpt 11, Loops. Calculate the expected pay-rate of the slot machine
 # copied code to create the slot machine
+library(tidyverse)
 get_symbols<- function(){
   wheel<- c("DD", '7', 'BBB',"BB", "B", "C", '0')
   sample(wheel, size =3, replace = TRUE,
@@ -91,14 +92,11 @@ chars
 #create  a for loop for calculating the prize in each row of slot_results. 
 slot_results$prize <- NA
 head(slot_results, 3)
-for (i in 1:nrow(slot_results)){
-  symbols <- c(slot_results[i,1], slot_results[i,2], slot_results[i,3])
-  slot_results$prize[i] <-score(symbols)
-}
+
 #expected payout from playing. sum of slot_results$prize * prob of winning that prize. 
 
 sum(slot_results$prize * slot_results$probability)
-slot_results[286,]
+#slot_results
 # try to handle write a version of score where DD is both wild and doubler.
 #can't figure out on my own yet, learn from book code. 
 symbols<- get_symbols()
@@ -108,11 +106,11 @@ score<- function(symbols){
   cherries <- sum(symbols == "C")
   #sum number of diamonds in a row
   slots <- symbols[symbols != "DD"]
-  same <- length(unique(slots)== 1)
+  same <- length(unique(slots) == 1)
   bars <- slots %in% c("B", "BB", "BBB")
   
   #get prize
-  if(diamonds ==3){
+  if(diamonds == 3){
     prize <- 100
   } else if(same) { 
     payouts_same <- c('7'= 80, 'BBB' = 40,"BB" = 25, 
@@ -120,19 +118,104 @@ score<- function(symbols){
     prize<- unname(payouts_same[slots[1]])
   } else if (all(bars)) { 
     prize <- 5 
-  } else if (cherries >0){
-    prize<- c(0,2,5)[cherries + diamonds + 1]
+  } else if (cherries > 0){
+    prize <- c(0,2,5)[cherries + diamonds + 1]
   } else {
     prize <- 0 }
   
 
-  prize*2 ^ diamonds
+  prize*2^diamonds
 }
 score(symbols)
+head(slot_results, 25)
 # recalculate the expected values:
 for (i in 1:nrow(slot_results)){
-  symbols <- c(slot_results[i,1], slot_results[i,2], slot_results[i,3])
-  slot_results$prize[i] <- score(symbols)
+  symbols_2 <- c(slot_results[i,1], slot_results[i,2], slot_results[i,3])
+  slot_results$prize[i] <- score(symbols_2)
 }
 sum(slot_results$prize * slot_results$probability)
+head(slot_results, 25)
 #wrong, try again. 
+slot_results$prize
+slot_results$probability
+slot_results
+sum(slot_results$prize*slot_results$probability)
+#while loops
+plays_till_broke<- function(starts_with) {
+  cash <- starts_with
+  n <-0
+  while (cash >0){
+    cash <- cash-1 + play()
+    n<- n + 1
+  }
+  n
+}
+plays_till_broke(100)
+# Chapter 12, Speed
+
+abs_loop<- function(vec){
+  for (i in 1:length(vec)) {
+    if (vec[i] < 0){
+      vec[i] <- -vec[i]
+    }
+  }
+  vec
+}
+
+abs_set<- function(vec){
+  negs<- vec<0
+  vec[negs]<- vec[negs]*-1
+  vec
+}
+long<- rep(c(-1,1),5000000)
+system.time(abs_loop(long))
+system.time((abs_set(long)))
+#R comes with built in absolute value function abs()
+system.time(abs(long))
+
+# vectorize this code
+
+change_symbols <- function(vec){
+  for (i in 1:length(vec)){
+    if (vec[i] == "DD") {
+      vec[i] <- "joker"
+    } else if (vec[i] == "C") {
+      vec[i] <- "ace"
+    } else if (vec[i] == "7") {
+      vec[i] <- "king"
+    }else if (vec[i] == "B") {
+      vec[i] <- "queen"
+    } else if (vec[i] == "BB") {
+      vec[i] <- "jack"
+    } else if (vec[i] == "BBB") {
+      vec[i] <- "ten"
+    } else {
+      vec[i] <- "nine"
+    } 
+  }
+  vec
+}
+
+vec <- c("DD", "C", "7", "B", "BB", "BBB", "0")
+
+change_symbols(vec)
+many <- rep(vec, 1000000)
+
+system.time(change_symbols(many))
+
+# my  version
+change_symbols <- function(vec){
+  cards<- c("DD" = "joker", "C" = "ace", "7" = "king", "B" = "queen", 
+            "BB" = "jack", "BBB" = "ten", "0" = "nine")
+  unname(cards[vec])
+  }
+change_symbols(vec)
+system.time(change_symbols(vec))
+
+# simulation
+winnings<- vector(length = 10000000)
+for (i in 1:1000000) {
+  winnings[i] <- play()
+}
+mean(winnings)
+#12.4
